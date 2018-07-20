30bd859ba45f0d9f43bdac508a8ebcd0c1ac5d0c
Jesse Francis <jesse.francis1@ucalgary.ca>
2018-07-20 17:15:11 -0600

    Done first row of printing

--- Printer.java
+++ Printer.java
@@ -1,5 +1,71 @@
 public class Printer {
-    private final String blockChar = "\u2588";
-    private final int blockWidth = 2;
-    private final String blockChar = "\u2588";
+
+    private final int BOARDWIDTH = 10;
+    private final int BOARDHEIGHT = 24;
+    // How many characters wide each block should be
+    private final int BLOCKWIDTH = 2;
+    private final int BOARDCHARWIDTH = BOARDWIDTH * BLOCKWIDTH;
+    // Block Character
+    private final String BLOCKCHAR = "\u2588";
+    // Vertical Character
+    private final String VCHAR = "\u2588";
+    // Horizontal Character
+    private final String HCHAR = "\u2554";
+    // Top Right Character
+    private final String TRCHAR = "\u2554";
+    // Top Left Character
+    private final String TLCHAR = "\u2554";
+    // Bottom Right Character
+    private final String BRCHAR = "\u2554";
+    // Bottom Left Character
+    private final String BLCHAR = "\u2554";
+    // Horizontal to Vertical Upper Split
+    private final String HVUSPLIT = "\u2554";
+    // Vertical to Horizontal Right Split
+    private final String VHRSPLIT = "\u2554";
+
+    public void print(Tetromino currentTetromino,
+                      Tetromino nextTetromino,
+                      Board board) {
+        
+    }
+
+    private Block[][] combine(Tetromino currentTetromino, Board board) {
+        Block[][] boardArray = board.getCurrentBoard();
+        Block[] tetromino = currentTetromino.getBlockArray();
+
+        for(Block block:tetromino) {
+            int x = block.getXPosition();
+            int y = block.getYPosition();
+            boardArray[y][x] = block;
+        }
+
+        return boardArray;
+    }
+
+    private String[] getHeader() {
+        String[] header = String[3];
+        String header1 = "";
+        for(int i =0; i <= BOARDCHARWIDTH * 3 / 4; i++) {
+            if(i < BOARDCHARWIDTH / 4) {
+                header1 += " ";
+            }
+            else if (i == BOARDCHARWIDTH / 4) {
+                header1 += TLCHAR;
+            }
+            else if (i > BOARDCHARWIDTH / 4 && i < BOARDCHARWIDTH * 3 / 4) {
+                header1 += HCHAR;
+            }
+            else if (i == BOARDCHARWIDTH * 3 / 4) {
+                header1 += TRCHAR;
+            }
+        }
+        header[0] = header1;
+        return header;
+    }
+
+    public void main(String[] args) {
+        System.out.println(getHeader()[0]);
+    }
+
 }
