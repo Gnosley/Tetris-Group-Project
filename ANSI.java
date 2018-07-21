dc96433d37fd58db849b02e405137030d5bfdd86
Jesse Francis <jesse.francis1@ucalgary.ca>
2018-07-20 20:11:59 -0600

    Add ability to clear console

--- ANSI.java
+++ ANSI.java
@@ -1,4 +1,5 @@
 public class ANSI {
+    public static final String CLEARSCREEN = "\u001B[1J";
     public static final String RESET = "\u001B[0m";
     public static final String BLACK = "\u001B[30m";
     public static final String RED = "\u001B[31m";
