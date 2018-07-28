b66edbc07572b3c52dd3c1983155a75d0a4d3dc6
Jesse Francis <jesse.francis1@ucalgary.ca>
2018-07-28 17:01:47 -0600

    Kinda working things

--- Tetromino.java
+++ Tetromino.java
@@ -89,13 +89,33 @@ public class Tetromino {
      *            old Tetromino that needs to be copied
      */
     public Tetromino(Tetromino tetromino) {
-		this.type = tetromino.getType();
+        this.type = tetromino.getType();
         this.xReferencePosition = tetromino.getXReference();
         this.yReferencePosition = tetromino.getYReference();
         this.tetrominoArray = tetromino.getBlockArray();
         this.size = tetromino.getSize();
     }
-	
+
+    public Tetromino(Tetromino tetromino, boolean isGhost) {
+        this.type = tetromino.getType();
+        this.xReferencePosition = tetromino.getXReference();
+        this.yReferencePosition = tetromino.getYReference();
+        this.tetrominoArray = tetromino.getBlockArray();
+        this.size = tetromino.getSize();
+        this.convertGhostType(isGhost);
+    }
+
+    private void convertGhostType(boolean toGhost) {
+        for(Block block:this.tetrominoArray) {
+            if(block != null){
+                block.setIsGhost(toGhost);
+            }
+        }
+    }
+
+    private Block[] generateTetrominoArray(int type) {
+        return generateTetrominoArray(type, false);
+    }
 
     /**
      * Generate the new tetromino thorough data from the tetrominoData array
@@ -103,7 +123,7 @@ public class Tetromino {
      * @param type:
      *            int of what type the tetromino is (i,o,t,s,z,j,l)
      */
-    private Block[] generateTetrominoArray(int type) {
+    private Block[] generateTetrominoArray(int type, boolean isGhost) {
         if (type < 0 || type > 6) type = 0; // only 7 tetromino types
 
         int color = tetrominoData[type][0][0];
@@ -116,7 +136,8 @@ public class Tetromino {
 
             Block block = new Block(color,
                                     xReferencePosition + x,
-                                    yReferencePosition + y);
+                                    yReferencePosition + y,
+                                    isGhost);
             tetrominoArray[i-1] = block;
         }
         return tetrominoArray;
