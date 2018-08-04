7234e16b449c251b640a864864c910a8e32a4041
Nglittleguy <41524588+Nglittleguy@users.noreply.github.com>
2018-08-04 16:41:41 -0600

    Tetromino Classes Fixed

---
+++ ITetromino.java
@@ -0,0 +1,22 @@
+
+    
+public class ITetromino extends Tetromino {
+    
+    private Block[] tetrominoArray = new Block[4];
+    
+    /**
+     * Constructor for a new I Tetromino when one is called for.
+     * @param xRef:
+     *            x-coordinate of reference position
+     * @param yRef:
+     *            y-coordinate of reference position
+     */
+    public ITetromino (int xRef, int yRef) {
+        super(xRef, yRef);
+        tetrominoData = {{1, 0}, {1, 1}, {1, 2}, {1, 3}};
+        size = 4;
+        type = 0;
+        setBlockArray(generateTetrominoArray(false));
+    }
+    
+}
---
+++ TTetromino.java
@@ -0,0 +1,21 @@
+
+    
+public class TTetromino extends Tetromino {
+    
+    private Block[] tetrominoArray = new Block[4];
+
+    /**
+     * Constructor for a new T type Tetromino when one is called for.
+     * @param xRef:
+     *            x-coordinate of reference position
+     * @param yRef:
+     *            y-coordinate of reference position
+     */ 
+    public TTetromino (int xRef, int yRef) {
+        super(xRef, yRef);
+        tetrominoData = {{1, 0}, {1, 1}, {0, 1}, {2, 1}};
+        size = 3;
+        type = 2;
+        setBlockArray(generateTetrominoArray(false));
+    }
+}
