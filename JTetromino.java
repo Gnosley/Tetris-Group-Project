e301e17cb29e7c0b4e67c1b403ae8e6be1722e40
Nglittleguy <41524588+Nglittleguy@users.noreply.github.com>
2018-08-04 17:31:02 -0600

    This is everything so that it works... I hope

---
+++ JTetromino.java
@@ -0,0 +1,21 @@
+
+    
+public class JTetromino extends Tetromino {
+    
+    private Block[] tetrominoArray = new Block[4];
+
+    /**
+     * Constructor for a new J type Tetromino when one is called for.
+     * @param xRef:
+     *            x-coordinate of reference position
+     * @param yRef:
+     *            y-coordinate of reference position
+     */ 
+    public JTetromino (int xRef, int yRef) {
+        super(xRef, yRef);
+        tetrominoData = new int[][] {{2, 0}, {1, 0}, {1, 1}, {1, 2}};
+        size = 3;
+        type = 5;
+        setBlockArray(generateTetrominoArray(false));
+    }
+}
