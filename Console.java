bb2851840725d1db0d1b42d1eecb691d876b3100
Jesse Francis <jesse.francis1@ucalgary.ca>
2018-08-05 22:12:00 -0600

    Enable auto-dropping.

--- Console.java
+++ Console.java
@@ -1,11 +1,32 @@
+import java.util.Timer;
+import java.util.TimerTask;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
 import org.jline.utils.NonBlockingReader;
 import java.io.IOException;
 
 public class Console{
+
+    public class DropTimer extends TimerTask {
+        @Override
+        public void run () {
+            // boolean gameDone = game.getBoard().isGameDone();
+            if(!isGameDone) {
+                game.tryMove('s');
+                isGameDone = game.getBoard().isGameDone();
+                printGame();
+            } else {
+                printGame();
+                this.cancel();
+            }
+        }
+    }
     private Terminal terminal;        // the terminal and
     private NonBlockingReader reader; // reader for instant input
+    Printer printer = new Printer();
+    Game game = new Game();
+    boolean isGameDone = false;
+    TimerTask dropTimer = new DropTimer();
 
     /**
      * Console constructor
@@ -30,23 +51,24 @@ public class Console{
         // Board board = new Board();
         // The printer object, which is what will produce graphics for text
         // based game
-        Printer printer = new Printer();
-        Game game = new Game();
 
         // Print initial board.
-        console.printGame(printer, game);
-        boolean isGameDone = false;
-        while(!isGameDone) {
+        console.printGame();
+        console.dropPieces();
+       // jgame.dropPieces();
+        while(!console.getIsGameDone()) {
             try{
                 int keyInASCII = console.reader.read();
                 if (keyInASCII == 'h') {
+                    console.pauseDropping();
                     keyInASCII = -1;
                     //pressing 'h' during game opens help/controls menu
-                    printer.printHelp(console.terminal);
+                    console.printer.printHelp(console.terminal);
                     while(keyInASCII == -1) {
                         keyInASCII = console.reader.read();
                     }
-                    console.printGame(printer, game);
+                    console.dropPieces();
+                    console.printGame();
                 }
                 // check if user wants to quit
                 if (keyInASCII == 27) {
@@ -59,23 +81,24 @@ public class Console{
                 }
                 else {
                     //if user enters valid keyboard command, game begins sequence for cheking the move
-                    game.tryMove(keyInASCII);
+                    console.game.tryMove(keyInASCII);
                 }
             }
             catch(IOException e){
                 System.out.println("Couldn't read character for move.");
             }
-            console.printGame(printer, game);
-            isGameDone = game.getBoard().isGameDone();
+            console.printGame();
+            console.setIsGameDone(console.game.getBoard().isGameDone());
         }
         System.out.println("Game Over!");
+        System.exit(0);
     }
 
     /**
      * [printGame description]
      * @param printer [description]
      */
-    private void printGame(Printer printer, Game game){
+    private void printGame(){
         printer.print(game.getCurrentTetromino(),
                       game.getNextTetromino(),
                       game.getGhostTetromino(),
@@ -87,4 +110,27 @@ public class Console{
                       game.getIsHoldMoveAvailable(),
                       game.getPieceStats());
     }
+
+    /**
+     * Creates a new timer thread.
+     * Tetrominos will drop at a rate determined by the timer
+     */
+    public void dropPieces() {
+        this.dropTimer = new DropTimer();
+        new Timer().schedule(dropTimer
+                             , 0, 300);
+    }
+
+    public void pauseDropping() {
+        this.dropTimer.cancel();
+    }
+
+    public boolean getIsGameDone() {
+        return this.isGameDone;
+    }
+
+    public void setIsGameDone(boolean gameDone) {
+        this.isGameDone = gameDone;
+    }
+
 }
