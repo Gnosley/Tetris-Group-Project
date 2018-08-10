2db8a97856b592b3274e0b5c6c01c5954f888301
Jesse Francis <jesse.francis1@ucalgary.ca>
2018-08-10 15:17:52 -0600

    Clean up Block, do a JavaDoc

--- src/main/java/org/teamsix/Block.java
+++ src/main/java/org/teamsix/Block.java
@@ -1,15 +1,22 @@
 package org.teamsix;
 
-/** Block class is the blocks that make up the game pieces. Blocks store their color,
-	xPosition and yPosition all as integers. Get/set methods for all 3 instance variables	*/
-public class Block{
+/**
+ * Block class is the blocks that make up the game pieces. Blocks store their
+ * color, xPosition and yPosition all as integers. Get/set methods for all 3
+ * instance variables
+ */
+public class Block {
+
     private int color = 0;
     private int xPosition;
     private int yPosition;
     private boolean isGhost;
 
-/** Three parameter constructor for color, xPosition and yPosition.
-	@param color, @param xPosition, @param yPosition	*/
+    /**
+     * Three parameter constructor for color, xPosition and yPosition.
+     *
+     * @param color, @param xPosition, @param yPosition
+     */
     public Block(int color, int x, int y, boolean isGhost) {
         this.setXPosition(x);
         this.setYPosition(y);
@@ -17,53 +24,88 @@ public class Block{
         this.setIsGhost(isGhost);
     }
 
-/** Block copy constructor
-	@param block	*/
+    /**
+     * Block copy constructor
+     *
+     * @param block
+     */
     public Block(Block block) {
         this.color = block.getColor();
         this.xPosition = block.getXPosition();
         this.yPosition = block.getYPosition();
         this.isGhost = block.getIsGhost();
     }
-    /** Set method for color
-      @param color	*/
+
+    /**
+     * Set method for color
+     *
+     * @param color
+     */
     public void setColor(int color) {
         if (color >= 0 && color < 7) {
             this.color = color;
         }
     }
-    /** Get method for color
-        @return boolean representing if it is a ghost block or not*/
+
+    /**
+     * Get method for color
+     *
+     * @return boolean representing if it is a ghost block or not
+     */
     public boolean getIsGhost() {
         return this.isGhost;
     }
 
+    /**
+     * Setter method for whether a block is a ghost.
+     *
+     * @param isGhost Boolean indicating whether block is ghost or not.
+     */
     public void setIsGhost(boolean isGhost) {
         this.isGhost = isGhost;
     }
-/** Get method for color
-	@return color*/
+
+    /**
+     * Get method for color
+     *
+     * @return color
+     */
     public int getColor() {
         return this.color;
     }
 
-/** Set method for xPosition
-	@param x New xPosition	*/
+    /**
+     * Set method for xPosition
+     *
+     * @param x New xPosition
+     */
     public void setXPosition(int x) {
         xPosition = x;
     }
-/** Set method for yPosition
-	@param y New yPosition	*/
+
+    /**
+     * Set method for yPosition
+     *
+     * @param y New yPosition
+     */
     public void setYPosition(int y) {
         yPosition = y;
     }
-/** Get method for xPosition
-	@return xPosition	*/
+
+    /**
+     * Get method for xPosition
+     *
+     * @return xPosition
+     */
     public int getXPosition() {
         return xPosition;
     }
-/** Get method for yPosition
-	@return yPosition	*/
+
+    /**
+     * Get method for yPosition
+     *
+     * @return yPosition
+     */
     public int getYPosition() {
         return yPosition;
     }
